41d0544dfb18e172253b993ac74bc3e635b1ef89
20IT158 <20it158@charusat.edu.in>
2022-09-16 00:02:04 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Student from './components/Student';
 import StudentList from './components/StudentList';
 import Footer from './components/Footer';
 import Container from 'react-bootstrap/Container';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <NavigationBar/>
       <Container>
-        <Student />
-        <StudentList />
+      <Student/>
+      <StudentList/>
+        <Routes>
+      <Route path="student" element={<Student/>}/>
+      <Route path="listStudents" element={<StudentList/>}/>
+      </Routes>
       </Container>
-      <Footer />
+      <Footer/>
+      </Router>
     </div>
   );
 }
