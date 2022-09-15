07c0b48cc8463b83d1e9d542afbc585e2d42862f
20IT158 <20it158@charusat.edu.in>
2022-09-15 23:20:28 +0530

    Added Footer to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
           <Student/>
       <StudentList/>
+      <Footer/>
     </div>
   );
 }
